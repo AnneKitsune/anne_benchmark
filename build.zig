e64f201ba13732705c57f70bb06343a6125f1186
AnneKitsune <annekitsunefox@gmail.com>
2025-08-22 21:58:06 -0400

    add missing optimize arg

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const zig_mod = b.addModule("anne_benchmark", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const c_mod = b.createModule(.{
